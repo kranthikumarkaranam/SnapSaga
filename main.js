7f16f1ba056143be666b32f9a2fa0a47e98ef79b
kranthi kumar <2019271@iiitdmj.ac.in>
2023-07-31 20:28:15 +0530

    first commit

---
+++ main.js
@@ -0,0 +1,67 @@
+// change nav style on scroll
+window.addEventListener('scroll', () => {
+	document
+		.querySelector('nav')
+		.classList.toggle('window-scrolled', window.scrollY > 0);
+});
+
+// CONTACT BUTTONS (circular text buttons)
+const textButtons = document.querySelectorAll('.contact__btn');
+
+textButtons.forEach((textButton) => {
+	let text = textButton.querySelector('p');
+
+	text.innerHTML = text.innerHTML
+		.split('')
+		.map(
+			(character, index) =>
+				`<span style = "transform: rotate(${
+					index * 16
+				}deg)">${character}</span>`
+		)
+		.join('');
+});
+
+// SWIPER JS (gallery section)
+var swiper = new Swiper('.mySwiper', {
+	slidesPerView: 1,
+	spaceBetween: 20,
+	pagination: {
+		el: '.swiper-pagination',
+		clickable: true,
+	},
+	breakpoints: {
+		599: {
+			slidesPerView: 2,
+			spaceBetween: 40,
+		},
+		1023: {
+			slidesPerView: 3,
+			spaceBetween: 60,
+		},
+	},
+});
+
+const nav = document.querySelector('.nav__links');
+const openNavBtn = document.querySelector('#nav__toggle-open');
+const closeNavBtn = document.querySelector('#nav__toggle-close');
+
+const openNav = () => {
+	nav.style.display = 'flex';
+	openNavBtn.style.display = 'none';
+	closeNavBtn.style.display = 'inline-block';
+};
+
+openNavBtn.addEventListener('click', openNav);
+
+const closeNav = () => {
+	nav.style.display = 'none';
+	openNavBtn.style.display = 'inline-block';
+	closeNavBtn.style.display = 'none';
+};
+
+closeNavBtn.addEventListener('click', closeNav);
+
+nav.querySelectorAll('li a').forEach((navLink) => {
+	navLink.addEventListener('click', closeNav);
+});
